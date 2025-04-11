1942a73c09af4fd2906ea90d0a079f6b4c771df4
Beerspitnight <beerspitnight@gmail.com>
2025-04-11 03:12:13 -0400

    added constants reference

---
+++ learning_app/scripts/constants.py
@@ -0,0 +1,13 @@
+"""
+Shared constants for the image tagging application.
+"""
+
+# Art elements and principles constants
+ELEMENT_OPTIONS = [
+    "Line", "Shape", "Form", "Color", "Value", "Space", "Texture"
+]
+
+PRINCIPLE_OPTIONS = [
+    "Balance", "Emphasis", "Movement", "Pattern & Repetition", 
+    "Rhythm", "Proportion", "Variety", "Unity"
+]
